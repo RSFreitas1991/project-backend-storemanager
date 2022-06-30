02d9cf99b94b68e3373b617ed08f2028659526ba
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-06-30 08:03:27 -0300

    Abertura de PR

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
+// meu PR
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
