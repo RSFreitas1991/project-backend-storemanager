d013d290eb66b304629b0b4570aa9d834c2a31f5
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-07-05 10:09:39 -0300

    Iniciando o requisito 1

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
 
 const app = express();
+app.use(express.json());
+const db = mysql.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '123456Rafa*',
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const sql = `select * from StoreManager.products where id = ${id};`;
+    const result = await db.query(sql);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
 
-// meu PR
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
