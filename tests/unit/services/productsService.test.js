1969192bb715c92d28cac7c9f65e4eb48006b7ac
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-07-10 20:38:12 -0300

    Instalado Chai as Promised e feito novo teste no productsService

--- tests/unit/services/productsService.test.js
+++ tests/unit/services/productsService.test.js
@@ -1,13 +1,22 @@
-const { expect } = require('chai');
+const { expect, use } = require('chai');
+const chaiAsPromised = require('chai-as-promised')
 const productsService = require('../../../services/productsService')
+
+use(chaiAsPromised);
 
 const saleNotFound = 'Sale not found';
 
 describe('Testes da camada productsModel', function () {
   describe('#checkId', function () {
-    it('se ao receber um id inexistente retorna erro', async function () {
+    it('se ao receber um id existente retorna true', async function () {
       const result = await productsService.checkId(1);
       expect(result).to.be.equal(true);
+    })
+  })
+  describe('#checkId', function () {
+    it('se ao receber um id inexistente retorna erro', async function () {
+      const result = await productsService.checkId(1);
+      expect(productsService.checkId(99)).to.be.rejectedWith(saleNotFound)
     })
   })
 })
